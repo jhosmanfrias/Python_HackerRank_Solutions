f8aa6749bf9db4a9d9ce7f11216f96f1125c4562
jhosmanfrias <jhosmanfrias@gmail.com>
2022-01-29 15:04:11 -0300

    Update collections_counter.py

--- Collections/collections_counter.py
+++ Collections/collections_counter.py
@@ -1,6 +1,6 @@
 ####################################################################################################
 # Challenge -> https://www.hackerrank.com/challenges/collections-counter/problem?isFullScreen=true #
-# By        -> jhosmanfrias, Jan-202                                                               #
+# By        -> jhosmanfrias, Jan-2022                                                              #
 ####################################################################################################
 
 
