c01bba08f064dd5dd4d5524e509f5971b2113d8a
jhosmanfrias <jhosmanfrias@gmail.com>
2022-01-29 14:15:25 -0300

    ADD: sets challenges

---
+++ Sets/introduction_to_sets.py
@@ -0,0 +1,14 @@
+########################################################################################################
+# Challenge -> https://www.hackerrank.com/challenges/py-introduction-to-sets/problem?isFullScreen=true #
+# By        -> jhosmanfrias, Jan-202                                                                   #
+########################################################################################################
+
+def average(array):
+    
+    return sum(set(array))/len(set(array))
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
