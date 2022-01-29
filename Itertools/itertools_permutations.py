c283f0fc96628ab3b4156fc83e9582bab5067f4f
jhosmanfrias <jhosmanfrias@gmail.com>
2022-01-29 15:05:29 -0300

    Update itertools_permutations.py

--- Itertools/itertools_permutations.py
+++ Itertools/itertools_permutations.py
@@ -1,6 +1,6 @@
 #######################################################################################################
 # Challenge -> https://www.hackerrank.com/challenges/itertools-permutations/problem?isFullScreen=true #
-# By        -> jhosmanfrias, Jan-202                                                                  #
+# By        -> jhosmanfrias, Jan-2022                                                                 #
 #######################################################################################################
 
 
